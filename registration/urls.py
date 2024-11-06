d0a2694153900cb0ac53f274aee54967e9ba0507
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2024-11-05 16:37:22 -0800

    login for student and teacher

--- registration/urls.py
+++ registration/urls.py
@@ -3,8 +3,8 @@ from .views import register_exam, home, view_exams, login_view, logout_view
 
 urlpatterns = [
     path('', home, name='home'),  # Landing page
-    path('register/', register_exam, name='register_exam'),  # Registration page
-    path('exams', view_exams, name='view_exams'),
+    path('register_exam/', register_exam, name='register_exam'),  # Registration page
+    path('view_exams/', view_exams, name='view_exams'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('password_reset/', login_view, name='password_reset'),
