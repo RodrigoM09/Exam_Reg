5265786e4ae6789ef949915076aa83d9ab837837
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2024-11-25 18:03:52 -0800

    modified css for register button

--- registration/models.py
+++ registration/models.py
@@ -69,5 +69,16 @@ class Exam(models.Model):
 
     def __str__(self):
         return self.exam_name
+class Registered(models.Model):
+    registration_id = models.AutoField(primary_key=True)
+    student_id = models.BigIntegerField()
+    exam_id = models.BigIntegerField()
+    username = models.CharField(max_length=50, null=True)
+    first_name = models.CharField(max_length=50, null=True)
+    last_name = models.CharField(max_length=50, null=True)
+    exam_name = models.CharField(max_length=255, null=True)
+    exam_date = models.DateField(null=True)
+    exam_location = models.CharField(max_length=255, null=True)
 
-
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.student_id}) - {self.exam_name}"
